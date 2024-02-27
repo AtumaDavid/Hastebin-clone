983bd38d176f0586f2b6bcb6a3b55f06cafcad06
AtumaDavid <atumadavid35@gmail.com>
2024-02-27 08:37:28 +0100

    project completed

--- server.js
+++ server.js
@@ -6,23 +6,40 @@ app.use(express.urlencoded({ extended: true }));
 
 const Document = require("./models/Document");
 const mongoose = require("mongoose");
+// mongoose.connect(
+//   "mongodb://localhost:27017/hastebin-clone1",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("connected");
+//     }
+//   }
+// );
 mongoose
-  .connect("mongodb://localhost/hastebin-clone", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://atumadavid35:atuma@cluster0.putsgrb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("Connected to MongoDB");
   })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
   });
 
 app.get("/", (req, res) => {
   //   res.send("code-display");
   const code = `Welcome to wasteBin Clone!
 Use the commands in the top right corner to create a new file to share with others`;
-  res.render("code-display", { code });
+  res.render("code-display", { code, language: "plaintext" });
 });
 
 app.get("/new", (req, res) => {
@@ -37,7 +54,27 @@ app.post("/save", async (req, res) => {
   } catch (error) {
     res.render("new", { value });
   }
-  console.log(value);
+  //   console.log(value);
+});
+
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("new", { value: document.value });
+  } catch (error) {
+    res.redirect(`/${id}`);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("code-display", { code: document.value, id });
+  } catch (error) {
+    res.redirect("/");
+  }
 });
 
 app.listen(3000);
